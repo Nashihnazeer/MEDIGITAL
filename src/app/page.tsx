0653d2d2610a2a6926b776b2b7cc5b65726c86ac
Our Mac <ourmac@Ours-MacBook-Air.local>
2025-09-26 15:06:50 +0530

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import HorizontalScrollWebsite from "@/components/HorizontalScrollWebsite";
+
+export default function Home() {
+  return (
+    <main className="overflow-x-hidden overflow-y-auto">
+      <HorizontalScrollWebsite />
+    </main>
+  );
+}
